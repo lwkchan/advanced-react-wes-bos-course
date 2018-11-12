7285a8da8cc66508fb76a831beaf218c51bf94f1
lwkchan <laurachan205@gmail.com>
2018-11-12 19:45:25 +0000

    finish mod3, ex12

---
+++ sick-fits/backend/src/db.js
@@ -0,0 +1,11 @@
+// Connects ot remote Prisma DB and allows us to query it
+const { Prisma } = require("prisma-binding");
+
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
